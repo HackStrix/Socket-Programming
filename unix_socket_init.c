a1184e36c7c459a337046c93bf896a38d2c4c1a8
HackStrix <hackstrix99@gmail.com>
2022-08-06 15:55:37 -0400

    Socket initialised for unix and cross-platform

---
+++ unix_socket_init.c
@@ -0,0 +1,19 @@
+
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <stdio.h>
+
+int main(){
+	printf("Socket ready to start");
+	return 0;
+}
+
+
